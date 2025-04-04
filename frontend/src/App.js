462c3cf7ba6222a1b50b6da0fdbfad02e3a42169
Heshan Rathnapala <43751413+hrathnapala@users.noreply.github.com>
2025-04-04 23:49:48 +0530

    placeholder centering

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -78,7 +78,6 @@ function App() {
           />
           <textarea
             placeholder="Description"
-            className="description-textarea"
             value={newTask.description}
             onChange={(e) =>
               setNewTask({ ...newTask, description: e.target.value })
